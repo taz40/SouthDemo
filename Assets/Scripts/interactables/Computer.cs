78bf1241c47d6e4bdc34698c31cb74824b19fe4e
Samuel Walker <108147@students.laramie1.org>
2016-02-10 09:51:17 -0700

    working on computer

---
+++ Assets/Scripts/interactables/Computer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Computer : MonoBehaviour, Interactable {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Interactable.use(GameObject user){
+		anim.SetTrigger("Used");
+	}
+}
