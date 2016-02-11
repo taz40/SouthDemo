ff1bbdd9fb233b1376be567450cd891e3350582a
Samuel Walker <108147@students.laramie1.org>
2016-02-11 11:07:17 -0700

    working on schedual and objective system

--- Assets/Scripts/LevelScripts/ExplorationGamemode.cs
+++ Assets/Scripts/LevelScripts/ExplorationGamemode.cs
@@ -3,6 +3,22 @@ using System.Collections;
 
 public class ExplorationGamemode : MonoBehaviour {
 
+	string[,] schedule = {
+		 {
+			"English 9",
+			"Alg 1",
+			"Physical Education 3",
+			"Discovering Pathways",
+			"US History",
+			"Spanish 1",
+			"Physical Science",
+			"Computer Applications Microsoft Office"
+		}, {"Geometry", "Spanish 2", "Biology", "English 10", "Health 4", "World Geography", "Hardware Matinence", "Programming in Visual Basic"},
+						{"Chemistry 1", "Webpage authering 1", "College and business apps", "Spanish 3", "English 11", "Alg 2", "US and Wyo Government", "Comuter graphics 1"},
+		{"Computer Graphics 3", "Chemistry 2", "English 12", "off", "College alg. and trig.", "Spanish 4", "Webpage authering 3", "off"}};
+
+
+
 	// Use this for initialization
 	void Start () {
 		PhotonNetwork.JoinRandomRoom ();
