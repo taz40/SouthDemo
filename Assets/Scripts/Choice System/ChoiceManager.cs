fbcf392736a3a22b2c11b0287e2fb9386b1df5d8
Samuel Walker <Samuel Walker>
2016-03-16 16:54:46 -0600

    working choice system

--- Assets/Scripts/Choice System/ChoiceManager.cs
+++ Assets/Scripts/Choice System/ChoiceManager.cs
@@ -7,6 +7,7 @@ public class ChoiceManager : MonoBehaviour {
 	public GameObject choicePrefab;
 	public GameObject optionPrefab;
 	public GameObject choicePos;
+    public GameObject optionPos;
     // Use this for initialization
     public static ChoiceManager instance;
 	void Start () {
@@ -27,8 +28,27 @@ public class ChoiceManager : MonoBehaviour {
         showChoice(choice);
     }
 
-	public void OptionPicked(Text text){
-		
+    public void OptionPicked(string text)
+    {
+        if (text == "Done")
+        {
+            PlayerController.canMove = true;
+            GameObject.Destroy(choicePos.transform.GetChild(0).gameObject);
+        }
+        else {
+            Option o = null;
+            foreach (Option o1 in this.choice.Options)
+            {
+                if (o1.Description == text)
+                    o = o1;
+            }
+            Choice choice = new Choice();
+            choice.Description = o.Response;
+            Option option1 = new Option();
+            option1.Description = "Done";
+            choice.Options.Add(option1);
+            showChoice(choice);
+        }
 	}
 	
 	// Update is called once per frame
@@ -37,9 +57,21 @@ public class ChoiceManager : MonoBehaviour {
 	}
 
 	public void showChoice(Choice choice){
-        PlayerController.cursorLocked = false;
-		GameObject choiceGO = (GameObject)GameObject.Instantiate (choicePrefab);
+        PlayerController.canMove = false;
+        if(choicePos.transform.childCount > 0)
+            GameObject.Destroy(choicePos.transform.GetChild(0).gameObject);
+        GameObject choiceGO = (GameObject)GameObject.Instantiate (choicePrefab);
 		choiceGO.transform.SetParent (choicePos.transform, false);
 		choiceGO.transform.FindChild ("Desc").GetComponent<Text> ().text = choice.Description;
+        int Offset = 0;
+        foreach (Option o in choice.Options) {
+            GameObject OptionGO = (GameObject)GameObject.Instantiate(optionPrefab);
+            OptionGO.transform.SetParent(choiceGO.transform.FindChild("OptionPositioning"), false);
+            OptionGO.transform.Translate(0, Offset, 0);
+            Offset -= 30;
+            OptionGO.transform.FindChild("Text").GetComponent<Text>().text = o.Description;
+            string optionName = o.Description;
+            OptionGO.GetComponent<Button>().onClick.AddListener(() => { OptionPicked(optionName); });
+        }
 	}
 }
